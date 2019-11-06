e54f6bd9133ab5e39d200f7b6ab29ff301503431
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-11-06 13:12:48 -0800

    Added components and imports/exports

---
+++ src/components/PhotoCard.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+
+function PhotoCard() {
+
+  return (
+    <div className='photo'>
+      
+    </div>
+  )
+}
+
+export default PhotoCard;
---
+++ src/components/PhotoList.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import PhotoCard from './PhotoCard';
+
+function PhotoList() {
+
+  return (
+     <div className='photos-container'>
+
+    </div>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+export default PhotoList;

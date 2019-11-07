753dcb21df8cd339f012b5d256c4679a2e7f92f2
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-11-07 12:44:03 -0800

    Refactored the buttons just a lil bit

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -13,29 +13,51 @@ function PhotoList() {
   useEffect( () => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=XscPqNahxbapakq5OHKTCSm3Hdyt7IBVf7ZUljNO&date=${date}`)
       .then( response => {
-        console.log(response);
+        // console.log(response);
         setPhoto(response.data);
       })
       .catch( error => console.log('Oops!', error));
   }, [date])
 
+  function prevPic(){
+    setDateIndex(dateIndex - 1)
+    console.log(dateIndex);
+    setDate(dateArray[dateIndex]);
+  }
+
+  function nextPic(){
+    setDateIndex(dateIndex + 1)
+    console.log(dateIndex);
+    setDate(dateArray[dateIndex]);
+  }
+
+  function changePic(amount){
+    setDateIndex(dateIndex + amount)
+    console.log(dateIndex);
+    setDate(dateArray[dateIndex]);
+  }
+
+
   return (
      <div className='photos-container'>
        <h2>{photo.title}</h2>
-       <h3>{photo.copyright} {parseDate(date)}</h3>
+       <h3>{photo.copyright}</h3> 
+       <h3>{parseDate(date)}</h3>
+
        <div className='button-container'>
          <button className='previous change-img-button' 
                 onClick={ () => {
-                  setDateIndex(dateIndex - 1);
-                  setDate(dateArray[dateIndex])
+                  // prevPic();
+                  changePic(-1);
                 }}>Previous day's photo</button>
          <button className='next change-img-button'
                 onClick={ () => {
-                  setDateIndex(dateIndex + 1);
-                  setDate(dateArray[dateIndex])
+                  // nextPic();
+                  changePic(1);
                 }}
                 >Next day's photo</button>
        </div>
+
        <img src={photo.url}/>
        <p>{photo.explanation}</p>
     </div>
@@ -96,10 +118,6 @@ function parseDate(dateStr) {
   
   day = removeLeadingZero(day);
   return `${day} ${month} ${year}`
-
-  //TO DO!
-
-//   console.log(newStr);
 }
 
 function removeLeadingZero(str){
@@ -110,11 +128,5 @@ function removeLeadingZero(str){
     return str;
   }
 }
-
-
-
-
-
-
 
 export default PhotoList;
